e0b46fc241eb37095ea34154e7c6de0f24194286
brenoroosevelt <breno.roosevelt@gmail.com>
2022-06-19 21:31:43 -0400

    test deploy

--- src/toast.ts
+++ src/toast.ts
@@ -1,64 +1,88 @@
-type ToastConfig = {
-    position?: 'top' | 'bottom',
-    justify?: 'end' | 'start' | 'center',
+type ToastPosition = 'top' | 'bottom'
+type ToastJustify = 'start' | 'center' | 'end'
+
+type ToastType = {
+    type: string,
+    closeBtn: boolean,
+    duration: number,
+    bgColor: string,
+    color: string,
+}
+
+type ToastCreation = {
+    position?: ToastPosition,
+    justify?: ToastJustify,
+    type?: string,
     closeBtn?: boolean,
     duration?: number,
     bgColor?: string,
-    color?: string
-}
-
-type ToastCreateConfig = {
-    duration?: number | undefined,
-    closeBtn?: boolean | undefined,
-    bgColor?: string | undefined,
-    color?: string | undefined,
+    color?: string,
 }
 
 class Toast {
-    private static _topLeft: Toast | null = null;
-    private static _topCenter: Toast | null = null;
-    private static _topRight: Toast | null = null;
-    private static _bottomLeft: Toast | null = null;
-    private static _bottomRight: Toast | null = null;
-    private static _bottomCenter: Toast | null = null;
 
-    private container: Element | null = null
+    private static defaultPosition: ToastPosition = 'top'
+    private static defaultJustify: ToastJustify = 'center'
 
-    private readonly config: ToastConfig = {
-        position: 'top',
-        justify: 'end',
-        closeBtn: true,
-        duration: 5000,
-        bgColor: '#333',
-        color: '#fff'
+    private container: HTMLDivElement | null = null
+
+    private static types = new Map<string, ToastType>([
+        ['default', {type: 'default', bgColor: '#333', color: '#fff', closeBtn: true, duration: 8000}],
+        ['info', {type: 'success', bgColor: 'teal', color: '#fff', closeBtn: true, duration: 8000}],
+        ['success', {type: 'success', bgColor: 'green', color: '#fff', closeBtn: true, duration: 8000}],
+        ['warning', {type: 'warning', bgColor: 'orange', color: '#fff', closeBtn: true, duration: 8000}],
+        ['error', {type: 'error', bgColor: 'red', color: '#fff', closeBtn: true, duration: 8000}],
+    ])
+
+    private static instances = new Map<string, Toast>([
+        ['top-start', new Toast("top", "start")],
+        ['top-center', new Toast("top", "center")],
+        ['top-end', new Toast("top", "end")],
+        ['bottom-start', new Toast("bottom", "start")],
+        ['bottom-center', new Toast("bottom", "center")],
+        ['bottom-end', new Toast("bottom", "end")],
+    ])
+
+    constructor(
+        private readonly position: 'top' | 'bottom' = Toast.defaultPosition,
+        private readonly justify: 'start' | 'center' | 'end' = Toast.defaultJustify,
+    ) {
     }
 
-    constructor(config: ToastConfig = {position: "top", justify: "end"}) {
-        this.config = {...this.config, ...config}
+    public static setType(anType: ToastType): void {
+        Toast.types.set(anType.type, anType)
     }
 
-    private createContainer(): Element {
+    private createContainer(): HTMLDivElement {
         const container = document.createElement('div')
         container.style.fontFamily = 'sans-serif';
         container.style.fontSize = '1rem';
         container.style.position = 'fixed';
-        container.style[this.config.position || 'top'] = '0'; // aqui
+        container.style[this.position] = '0';
         container.style['left'] = '0';
         container.style.width = '100%';
         container.style.margin = '1rem 0';
         container.style.padding = '0';
         container.style.display = 'grid';
-        container.style.justifyItems = this.config.justify || 'end'; // aqui
+        container.style.justifyItems = this.justify;
         container.style.gap = '0.5rem';
         container.style.pointerEvents = 'none'
 
         return container
     }
 
-    public create(message: string, config: ToastCreateConfig = {}): Element {
-        const _config = {...this.config, ...config}
+    public create(message: string, config: ToastType | object = {}): HTMLDivElement {
+        let type: ToastType = <ToastType>Toast.types.get('default')
+        if ('type' in config && Toast.types.has(config['type'])) {
+            type = <ToastType>Toast.types.get(config['type'])
+        }
+        const _config: ToastType = {...type, ...config}
+
         if (!(this.container instanceof Element)) {
-            this.container = document.body?.insertAdjacentElement('afterbegin', this.createContainer());
+            this.container = <HTMLDivElement>document.body?.insertAdjacentElement(
+                'afterbegin',
+                this.createContainer()
+            );
         }
 
         const toaster = document.createElement('div')
@@ -86,7 +110,7 @@ class Toast {
         return toaster
     }
 
-    private createCloseButton(): Element {
+    private createCloseButton(): HTMLDivElement {
         const closeBtn = document.createElement('div')
         closeBtn.style.cursor = 'pointer'
         closeBtn.style.background = 'rgba(0,0,0,0.1)'
@@ -102,91 +126,33 @@ class Toast {
         return closeBtn
     }
 
-    set position(v: 'top' | 'bottom') {
-        this.config.position = v
+    public static default(message: string, config: ToastCreation = {}): HTMLDivElement {
+        const _position: ToastPosition = config.position || Toast.defaultPosition
+        const _justify: ToastJustify = config.justify || Toast.defaultJustify
+        const defaultInstance = Toast.instances.get('top-center')
+        const instance: Toast = (<Toast>Toast.instances.get(_position + '-' +_justify))
+        return (instance || defaultInstance).create(message, config)
     }
 
-    set justify(v: 'start' | 'center' | 'end') {
-        this.config.justify = v
+    public static info(message: string, config: ToastCreation = {}): HTMLDivElement {
+        return Toast.default(message, {...config, ...{type: 'info'}})
     }
 
-    set bgColor(v: string) {
-        this.config.bgColor = v
+    public static warning(message: string, config: ToastCreation = {}): HTMLDivElement {
+        return Toast.default(message, {...config, ...{type: 'warning'}})
     }
 
-    set color(v: string) {
-        this.config.color = v
+    public static error(message: string, config: ToastCreation = {}): HTMLDivElement {
+        return Toast.default(message, {...config, ...{type: 'error'}})
     }
 
-    set closeBtn(v: boolean) {
-        this.config.closeBtn = v
+    public static success(message: string, config: ToastCreation = {}): HTMLDivElement {
+        return Toast.default(message, {...config, ...{type: 'success'}})
     }
 
-    set duration(v: number) {
-        this.config.duration = v
-    }
-
-    get position() {
-        return <'top'|'bottom'>this.config.position
-    }
-
-    get justify() {
-        return <'start' | 'center' | 'end'>this.config.justify
-    }
-
-    get bgColor() {
-        return <string>this.config.bgColor
-    }
-
-    get color() {
-        return <string>this.config.color
-    }
-
-    get closeBtn() {
-        return <boolean>this.config.closeBtn
-    }
-
-    get duration() {
-        return <number>this.config.duration
-    }
-
-    public info(message: string, config: ToastCreateConfig = {}): Element {
-        return this.create(message, {...config, ...{bgColor: "#333"}})
-    }
-
-    public warning(message: string, config: ToastCreateConfig = {}): Element {
-        return this.create(message, {...config, ...{bgColor: "orange"}})
-    }
-
-    public error(message: string, config: ToastCreateConfig = {}): Element {
-        return this.create(message, {...config, ...{bgColor: "red"}})
-    }
-
-    public success(message: string, config: ToastCreateConfig = {}): Element {
-        return this.create(message, {...config, ...{bgColor: "teal"}})
-    }
-
-    public static topLeft(config: ToastConfig = {}): Toast {
-        return Toast._topLeft || (Toast._topLeft = new Toast({...config, ...{position: "top", "justify" : "start"}}))
-    }
-
-    public static topRight(config: ToastConfig = {}): Toast {
-        return Toast._topRight || (Toast._topRight = new Toast({...config, ...{position: "top", "justify" : "end"}}))
-    }
-
-    public static topCenter(config: ToastConfig = {}): Toast {
-        return Toast._topCenter || (Toast._topCenter = new Toast({...config, ...{position: "top", "justify" : "center"}}))
-    }
-
-    public static bottomLeft(config: ToastConfig = {}): Toast {
-        return Toast._bottomLeft || (Toast._bottomLeft = new Toast({...config, ...{position: "bottom", "justify" : "start"}}))
-    }
-
-    public static bottomRight(config: ToastConfig = {}): Toast {
-        return Toast._bottomRight || (Toast._bottomRight = new Toast({...config, ...{position: "bottom", "justify" : "end"}}))
-    }
-
-    public static bottomCenter(config: ToastConfig = {}): Toast {
-        return Toast._bottomCenter || (Toast._bottomCenter = new Toast({...config, ...{position: "bottom", "justify" : "center"}}))
+    public static setDefaults(config: ToastCreation = {}): void {
+        Toast.defaultPosition = config.position || Toast.defaultPosition
+        Toast.defaultJustify = config.justify || Toast.defaultJustify
+        Toast.types.set('default', {...Toast.types.get('default'), ...<ToastType>config})
     }
 }
